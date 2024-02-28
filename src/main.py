a6b3bd49368c0c19344b9b2729aa4c0dda4783ce
Antonio <antonio.guimaraes@sou.inteli.edu.br>
2024-02-28 16:56:25 -0300

    fix: main, add mojej

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
 # Importando bibliotecas
 from serial.tools import list_ports
-import time
 import inquirer
 import pydobot
 from yaspin import yaspin
+
+# Classe para mover o robo com as juntas
+class InteliArm(pydobot.Dobot):
+    def __init__(self, port=None, verbose=False):
+        super().__init__(port=port, verbose=verbose)
+    
+    def movej_to(self, x, y, z, r, wait=True):
+        super()._set_ptp_cmd(x, y, z, r, mode=pydobot.enums.PTPMode.MOVJ_XYZ, wait=wait)
+
+    def movel_to(self, x, y, z, r, wait=True):
+        super()._set_ptp_cmd(x, y, z, r, mode=pydobot.enums.PTPMode.MOVL_XYZ, wait=wait)
+
+        
 
 # Declarando o spinner
 spinner = yaspin(text="Movimentando...", color="yellow")
@@ -23,7 +35,7 @@ def get_port():
 def go_home():
     print("Voltando para posição inicial...")
     spinner.start()
-    robo.move_to(240, 0, 150, 0, wait=True)
+    robo.movej_to(240, 0, 150, 0, wait=True)
     spinner.stop()
 
 # Função para ligar o atuador do robo
@@ -47,7 +59,7 @@ def turn_actuator_off():
 def move(x, y, z, r):
     spinner.start()
     print(f"Se movendo para a posição: ({x}, {y}, {z}), rotação: {r}")
-    robo.move_to(x, y, z, r, wait=True)
+    robo.movej_to(x, y, z, r, wait=True)
     spinner.stop()
     print(f"Movimento para a posição: ({x}, {y}, {z}), rotação: {r} concluído!")
 
@@ -96,7 +108,7 @@ def get_action():
 # Adquirir a porta serial escolhida
 port = get_port()
 
-robo = pydobot.Dobot(port=port, verbose=False)
+robo = InteliArm(port=port, verbose=False)
 robo.speed(30, 30)
 
 # Loop para pegar as ações escolhidas
@@ -104,6 +116,7 @@ while True:
     chosen_action, action_params = get_action()
     if chosen_action in action_functions:
         if chosen_action == "Sair":
+            robo.close()
             break
         elif action_params:
             action_functions[chosen_action](*action_params)
@@ -111,3 +124,4 @@ while True:
             action_functions[chosen_action]()
     else:
         print("Ação inválida")
+
